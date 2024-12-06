e650420ea5372deff40b2421169803ed8b653af6
Rishikesh Jawale <94427560+Rishikesh6641@users.noreply.github.com>
2024-12-07 00:34:12 +0530

    Add files via upload

---
+++ Project_BackEnd/Gamers_Castle_Backend/src/main/java/com/gc/GamersCastleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamersCastleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamersCastleBackendApplication.class, args);
+	}
+
+}
