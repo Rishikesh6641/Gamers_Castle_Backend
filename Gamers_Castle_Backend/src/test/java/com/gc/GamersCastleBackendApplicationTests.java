2ead5bae20c4e478b23952d84eeb276cd76aa032
Rishikesh6641 <rishikeshjawale@gmail.com>
2024-12-07 12:08:20 +0530

    added controller

---
+++ Gamers_Castle_Backend/src/test/java/com/gc/GamersCastleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamersCastleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
